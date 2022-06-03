725afccb9cd75fe1e64ad70266af63e0db55f9b5
Daiana Michels <daianaem.12@gmail.com>
2022-06-03 20:32:22 -0300

    feat: cria base da página minhas vendas

--- src/pages/Vendas/List/index.js
+++ src/pages/Vendas/List/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import {React, useState} from "react";
+import "./ListSale.css";
 
-// import { Container } from './styles';
+import TableSale from "../../../components/TableSale";
+import InputBusca from "../../../components/InputBusca";
+import ListHeader from "../../../components/ListHeader";
+import { Container } from "@mui/system";
 
 function VendaList() {
-  return <div />;
+  const [busca, setBusca]= useState('');
+  const onChange= (event) => {
+     
+    setBusca(event.target.value);
+
+    const lowerBusca = busca.toLowerCase();
+
+    const resultFilter = this.data.filter((data) => {
+      return data.nomeCliente.toLowerCase().includes(lowerBusca);
+    });
+
+    this.setState({
+      nomeCliente: resultFilter,
+    });
+  }
+ 
+  return (
+    <div className="container">
+      <Container sx={{ color: 'text.main', bgcolor: 'primary.main', width:'15%', height: '100%' }}>
+        <h1>Aqui vai o menu de navegação</h1>
+      </Container>
+      
+      <section className="container vendas">
+        <ListHeader paginaAtual="vendas" href={"/vendas"}></ListHeader>
+        <InputBusca placeholder={"Buscar por Cliente ..."} onChange={onChange}/>
+        <TableSale />
+      </section>
+    </div>
+  );
 }
 
 export default VendaList;
