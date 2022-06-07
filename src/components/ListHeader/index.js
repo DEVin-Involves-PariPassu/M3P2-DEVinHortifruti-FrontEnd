68075cce86010e676e27a8432195711ab221dcf4
Camilla <camillaamaral7@gmail.com>
2022-06-06 22:03:13 -0300

    feat: adiciona estilo da pagina UsuarioList

--- src/components/ListHeader/index.js
+++ src/components/ListHeader/index.js
@@ -1,45 +1,49 @@
 import React from "react";
-
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
-import {MdOutlineAddBusiness} from "react-icons/md";
-import {BsPersonPlus} from "react-icons/bs";
-import {MdOutlinePostAdd} from "react-icons/md";
+import { MdOutlineAddBusiness } from "react-icons/md";
+import { BsPersonPlus } from "react-icons/bs";
+import { MdOutlinePostAdd } from "react-icons/md";
 
 function ListHeader({ paginaAtual, onClick }) {
   return (
-    <Stack spacing={24} direction="row">
-      {paginaAtual === "vendas" && <h1>Vendas Cadastradas: </h1>}
-      {paginaAtual === "produtos" && <h1>Produtos Cadastrados: </h1>}
-      {paginaAtual === "usuarios" && <h1>Usuários Cadastrados: </h1>}
-      <Button
-        variant="contained"
-        color="variant"
-        size="medium"
-        onClick={onClick}
-      >
-        {paginaAtual === "vendas" && (
-          <>
-            <h3>NOVA VENDA</h3>
-            <MdOutlineAddBusiness />
-          </>
-        )}
+    <div style={{ display: "flex" }}>
+      <Stack direction="row" alignItems="center" spacing={40}>
+        {paginaAtual === "vendas" && <h1>Vendas Cadastradas</h1>}
+        {paginaAtual === "produtos" && <h1>Produtos Cadastrados</h1>}
+        {paginaAtual === "usuarios" && <h1>Usuários Cadastrados</h1>}
 
-        {paginaAtual === "usuarios" && (
-          <>
-            <h3>NOVO USUÁRIO</h3>
-            <BsPersonPlus />
-          </>
-        )}
-        {paginaAtual === "produtos" && (
-          <>
-            <h3>NOVO PRODUTO</h3>
-            <MdOutlinePostAdd />
-          </>
-        )}
-      </Button>
-    </Stack>
+        <Button
+          variant="contained"
+          color="variant"
+          size="medium"
+          style={{ borderRadius: "20px" }}
+          onClick={onClick}
+        >
+          {paginaAtual === "vendas" && (
+            <>
+              <h3>NOVA VENDA</h3>
+              <MdOutlineAddBusiness style={{ marginLeft: "10px" }} />
+            </>
+          )}
+
+          {paginaAtual === "produtos" && (
+            <>
+              <h3>NOVO PRODUTO</h3>
+              <MdOutlinePostAdd style={{ marginLeft: "10px" }} />
+            </>
+          )}
+
+          {paginaAtual === "usuarios" && (
+            <>
+              <h3>NOVO USUÁRIO</h3>
+              <BsPersonPlus style={{ marginLeft: "10px" }} />
+            </>
+          )}
+        </Button>
+      </Stack>
+    </div>
   );
 }
 
