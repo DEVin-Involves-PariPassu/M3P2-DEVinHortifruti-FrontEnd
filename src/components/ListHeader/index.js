c4de77d4ec339476f42a4a917075bbacfc093ff5
Kalyana Greim <kalygreim11@gmail.com>
2022-06-06 22:03:17 -0300

    feat : estiliza o cabeçalho da página

--- src/components/ListHeader/index.js
+++ src/components/ListHeader/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './ListHeader.css'
 
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
@@ -9,33 +10,35 @@ import {MdOutlinePostAdd} from "react-icons/md";
 
 function ListHeader({ paginaAtual, onClick }) {
   return (
-    <Stack spacing={24} direction="row">
-      {paginaAtual === "vendas" && <h1>Vendas Cadastradas: </h1>}
-      {paginaAtual === "produtos" && <h1>Produtos Cadastrados: </h1>}
-      {paginaAtual === "usuarios" && <h1>Usuários Cadastrados: </h1>}
+    <Stack spacing={28} direction="row" alignItems={'center'} width={'96%'}>
+      {paginaAtual === "vendas" && <h1 className="title">Vendas Cadastradas: </h1>}
+      {paginaAtual === "produtos" && <h1 className="title">Produtos Cadastrados: </h1>}
+      {paginaAtual === "usuarios" && <h1 className="title">Usuários Cadastrados: </h1>}
       <Button
         variant="contained"
         color="variant"
         size="medium"
+        hover
         onClick={onClick}
+        sx={{width: '170px', borderRadius: '25px', justifyContent: 'space-around', fontFamily:'Exo', fontSize: '0.7rem'}}
       >
         {paginaAtual === "vendas" && (
           <>
             <h3>NOVA VENDA</h3>
-            <MdOutlineAddBusiness />
+            <MdOutlineAddBusiness size={'22px'}/>
           </>
         )}
 
         {paginaAtual === "usuarios" && (
           <>
             <h3>NOVO USUÁRIO</h3>
-            <BsPersonPlus />
+            <BsPersonPlus size={'22px'}/>
           </>
         )}
         {paginaAtual === "produtos" && (
           <>
             <h3>NOVO PRODUTO</h3>
-            <MdOutlinePostAdd />
+            <MdOutlinePostAdd size={'22px'}/>
           </>
         )}
       </Button>
