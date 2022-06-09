20121e1f6acf738cfcb7f879a363b0dc12ab926f
Daiana Michels <daianaem.12@gmail.com>
2022-06-09 13:51:40 -0300

    feat: adiciona configuração inicial do token headers

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: `http://localhost:8081`,
+}
+
+);
+
+api.interceptors.request.use(async config => {
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTZWN1cml0eSBKV1QiLCJzdWIiOiJ7XHJcbiAgXCJpZFwiIDogMyxcclxuICBcIm5vbWVcIiA6IFwiQWRtaW5pc3RyYWRvciAyIERFVmluSG9ydGlmcnV0aVwiLFxyXG4gIFwibG9naW5cIiA6IG51bGwsXHJcbiAgXCJlbWFpbFwiIDogXCJsdWNhc19jZXNhcmlvQGVzdHVkYW50ZS5zZXNpc2VuYWkub3JnLmJyXCIsXHJcbiAgXCJkdE5hc2NpbWVudG9cIiA6IG51bGwsXHJcbiAgXCJpc0FkbWluXCIgOiB0cnVlXHJcbn0iLCJpYXQiOjE2NTQ3MTY2NzgsImV4cCI6MTY1NDgwMzA3OH0.YTKt3x1VxCDPRQbyZK8hjed-IjRAkMnZWDCmzlk4qeo";
+
+  if (token) {
+    api.defaults.headers.authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
