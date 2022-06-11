d2d14b9317d60932883e73132c147dd030ae00c2
Nesser Andrade <nesser24601@live.com>
2022-06-11 16:46:35 -0300

    adicionado api do viacep para preencher os campos da pagina DadosEntrega

--- src/pages/Vendas/Form/DadosEntrega/index.js
+++ src/pages/Vendas/Form/DadosEntrega/index.js
@@ -1,9 +1,9 @@
-import {React, useEffect, useState} from 'react';
+import { React, useEffect, useState } from "react";
+import "./styles.css";
 
 // import { Container } from './styles';
 
 function DadosEntrega() {
-
   const [CEP, setCEP] = useState([]);
   const [UF, setUF] = useState([]);
   const [cidade, setCidade] = useState([]);
@@ -23,89 +23,130 @@ function DadosEntrega() {
     setNumero(value);
   };
 
-  const handleChangeComplemento = (event) => {
-    const { value } = event.target;
-    setComplemento(value);
-  };
-
   const handleChangeDataEntrega = (event) => {
     const { value } = event.target;
     setDataEntrega(value);
   };
 
-  return (<>
-  <form className="formCreate" onSubmit={handleSubmit}>
-          <label>CEP:</label>
-          <input
-            type="number"
-            name="CEP"
-            value={CEP}
-            onChange={handleChangeCEP}
-            onBlur={handleDataCEP}
-            placeholder="Digite o CEP"
-            required
-          ></input>
+  const handleChangeComplemento = (event) => {
+    const { value } = event.target;
+    setComplemento(value);
+  };
 
-          <label>Estado:</label>
-          <input
-            type="text"
-            name="UF"
-            value={UF}
-            disabled
-            required
-          ></input>
+  async function handleDataCEP() {
+    if (CEP.length !== 8) {
+      return;
+    }
+    try {
+      const result = await fetch(`https://viacep.com.br/ws/${CEP}/json/`);
+      const dados = await result.json();
+      setLogradouro(dados.logradouro);
+      setComplemento(dados.complemento);
+      setBairro(dados.bairro);
+      setCidade(dados.localidade);
+      setUF(dados.uf);
+    }
+    catch {
 
-          <label>Cidade:</label>
-          <input
-            type="text"
-            name="cidade"
-            value={cidade}
-            disabled
-            required
-          ></input>
-          <label>Logradouro:</label>
-          <input
-            type="text"
-            name="logradouro"
-            value={logradouro}
-            disabled
-            required
-          ></input>
-          <label>Número:</label>
-          <input
-            type="number"
-            name="numero"
-            value={numero}
-            onChange={handleChangeNumero}
-            required
-          ></input>
-          <label>Bairro:</label>
-          <input
-            type="text"
-            name="bairro"
-            value={bairro}
-            disabled
-            required
-          ></input>
-          <label>Complemento:</label>
-          <input
-            type="text"
-            name="complemento"
-            value={complemento}
-            onChange={handleChangeComplemento}
-            required
-          ></input>
-          <label>Data de entrega:</label>
-          <input
-            type="text"
-            name="dataentrega"
-            value={dataEntrega}
-            onChange={handleChangeDataEntrega}
-            required
-          ></input>
-          <input className="sendLog" type="submit" value="Enviar" />
-          </form>
-  </>);
+    }    
+  }
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      event.target.checkValidity();
+
+      if (!CEP) {
+        alert("CEP é obrigatório");
+        return;
+      }
+
+      if (CEP.length !== 8) {
+        alert("CEP inválido");
+        return;
+      }
+
+      if (!numero) {
+        alert("Número é obrigatório");
+        return;
+      }
+
+      if (!dataEntrega) {
+        alert("Data de entrega é obrigatório");
+        return;
+      }
+    } catch {}
+  };
+  return (
+    <>
+    <div className="containerLog">
+      <form className="formLog" onSubmit={handleSubmit}>
+        <label>CEP:</label>
+        <input
+          type="number"
+          name="CEP"
+          value={CEP}
+          onChange={handleChangeCEP}
+          onBlur={handleDataCEP}
+          placeholder="Digite o CEP"
+          required
+        ></input>
+
+        <label>Estado:</label>
+        <input type="text" name="UF" value={UF} disabled required></input>
+
+        <label>Cidade:</label>
+        <input
+          type="text"
+          name="cidade"
+          value={cidade}
+          disabled
+          required
+        ></input>
+        <label>Logradouro:</label>
+        <input
+          type="text"
+          name="logradouro"
+          value={logradouro}
+          disabled
+          required
+        ></input>
+        <label>Número:</label>
+        <input
+          type="number"
+          name="numero"
+          value={numero}
+          onChange={handleChangeNumero}
+          required
+        ></input>
+        <label>Bairro:</label>
+        <input
+          type="text"
+          name="bairro"
+          value={bairro}
+          disabled
+          required
+        ></input>
+        <label>Complemento:</label>
+        <input
+          type="text"
+          name="complemento"
+          value={complemento}
+          onChange={handleChangeComplemento}
+        ></input>
+        <label>Data de entrega:</label>
+        <input
+          type="text"
+          name="dataentrega"
+          value={dataEntrega}
+          onChange={handleChangeDataEntrega}
+          required
+        ></input>
+        <input className="sendLog" type="submit" value="Enviar" />
+      </form>
+      </div>
+    </>
+  );
 }
 
 export default DadosEntrega;
