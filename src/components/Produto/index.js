425b7e3f70c8a7c885dd2f6d18503e22a2d97c25
Gesiel Neves <neves.gesiel@gmail.com>
2022-06-12 18:09:08 -0300

    Adicionado Pesquisa

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,10 +1,30 @@
 import TextField from "@mui/material/TextField";
 import DialogTitle from "@mui/material/DialogTitle";
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import Button from "@material-ui/core/Button";
+import EntradaPesquisa from "./pesquisa";
+import './styles.css';
 
 const Produto = () => {
+  const api = 'https://kitsu.io/api/edge/';
+  const [info, setInfo] = useState({});
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    if (text) {
+      setInfo({});
+
+      fetch(
+        `${api}anime?filter[text]=${text}&page[limit]=12`
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          setInfo(response);
+        });
+    }
+  }, [text]);
   return (    
+    <div className="produtos">
       <form>
         <DialogTitle id="alert-dialog-title">
           {"Nova venda: Produtos"}
@@ -23,6 +43,25 @@ const Produto = () => {
         </thead>
         <Button variant="outlined">Próxima etapa</Button>
       </form>
+      <EntradaPesquisa
+        value={text}
+        onChange={(search) => setText(search)}
+      />
+      {text && !info.data && <span>Carregando...</span>}
+      {info.data && (
+        <ul className="produtos-list">
+          {info.data.map((anime) => (
+            <li key={anime.id}>
+              <img
+                src={anime.attributes.posterImage.small}
+                alt={anime.attributes.canonicalTitle}
+              />
+              {anime.attributes.canonicalTitle}
+            </li>
+          ))}
+        </ul>
+      )}
+      </div>
     
   );
 };
