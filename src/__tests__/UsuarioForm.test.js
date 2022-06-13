46d061e6aa3ec2f9484e120f5a88652fbb338dc7
Edvan Junior <edvanbrito.j@hotmail.com>
2022-06-12 22:16:16 -0300

    Cria testes para o UsuarioForm

--- src/__tests__/UsuarioForm.test.js
+++ src/__tests__/UsuarioForm.test.js
@@ -2,13 +2,11 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import UsuarioForm from 'components/UsuarioForm';
 
-
-
 describe('UsuarioForm', ()=> {
     test('deve conter campo de texto para digitar nome do usuário', () => {
         render(<UsuarioForm/>);
 
-        const fieldNome = screen.getByPlaceholderText(/Login do Usuário/i)
+        const fieldNome = screen.getByPlaceholderText(/Nome/i)
         expect(fieldNome).toBeInTheDocument();
     });
 
@@ -16,7 +14,7 @@ describe('UsuarioForm', ()=> {
         render(<UsuarioForm/>);
 
         const fieldEmail = screen.getByPlaceholderText(/devin@hortifruti.com/i)
-        expect(fieldEmail).toBeInTheDocument();
+        expect(fieldEmail).toBeInTheDocument();   
     });
 
 });
